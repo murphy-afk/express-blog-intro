760c2adc72d53e56dd06632ff4b14a11e53f0cdb
murphy-afk <bardarenoemi@gmail.com>
2025-12-16 15:02:12 +0100

    feat: route '/'

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import express from 'express'
 const app = express();
 const port = 3005;
 
+app.get('/', (req, res) => {
+  res.send('Server del mio blog');
+})
+
 app.listen(port, function () {
   console.log("Server is listening on port: " + port);
 });
