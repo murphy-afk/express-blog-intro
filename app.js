92dfc0467cc0c134f72a0c2722b8a9edfb8744b3
murphy-afk <bardarenoemi@gmail.com>
2025-12-17 09:29:09 +0100

    feat: changed '/posts' to '/ bacheca'

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Server del mio blog');
 })
 
-app.get('/posts', (req, res) => {
+app.get('/bacheca', (req, res) => {
   const post = {
     info: {
       total: posts.length,
